7d294cd18e8d59976d341219a4435b8e099e02ec
mattr21 <matt.rothstein21@gmail.com>
2019-02-12 21:32:35 -0800

    styled everything - MVP complete

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -6,25 +6,46 @@ import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 
 const App = () => {
   return (
-    <div className='numbers'>
-      <NumberButton buttonStyles={'number'} text={1} />
-      <NumberButton buttonStyles={'number'} text={2} />
-      <NumberButton buttonStyles={'number'} text={3} />
-      <NumberButton buttonStyles={'number'} text={4} />
-      <NumberButton buttonStyles={'number'} text={5} />
-      <NumberButton buttonStyles={'number'} text={6} />
-      <NumberButton buttonStyles={'number'} text={7} />
-      <NumberButton buttonStyles={'number'} text={8} />
-      <NumberButton buttonStyles={'number'} text={9} />
-    </div>
-    <div className='operators'>
-      <NumberButton buttonStyles={'operator'} text={"/"} />
-      <NumberButton buttonStyles={'operator'} text={"x"} />
-      <NumberButton buttonStyles={'operator'} text={"-"} />
-      <NumberButton buttonStyles={'operator'} text={"+"} />
-      <NumberButton buttonStyles={'operator'} text={"="} />
-      {/* <ActionButton /> */}
-      {/* <CalculatorDisplay /> */}
+    <div className='calculator'>
+    
+      <div className='displays'>
+        <CalculatorDisplay buttonStyle={'display'} text={'0'} />
+      </div>
+
+      <div className='main'>
+        <div className='left-buttons'>
+          <div className='actions'>
+            <ActionButton buttonStyle={'action'} text={'clear'} />
+          </div>
+
+          <div className='numbers'>
+            <NumberButton buttonStyle={'number'} text={7} />
+            <NumberButton buttonStyle={'number'} text={8} />
+            <NumberButton buttonStyle={'number'} text={9} />
+            <NumberButton buttonStyle={'number'} text={4} />
+            <NumberButton buttonStyle={'number'} text={5} />
+            <NumberButton buttonStyle={'number'} text={6} />
+            <NumberButton buttonStyle={'number'} text={1} />
+            <NumberButton buttonStyle={'number'} text={2} />
+            <NumberButton buttonStyle={'number'} text={3} />
+          </div>
+
+          <div className='actions'>
+            <ActionButton buttonStyle={'action actionZero'} text={0} />
+          </div>
+        </div>
+
+        <div className='operators'>
+          <NumberButton buttonStyle={'operator'} text={'/'} />
+          <NumberButton buttonStyle={'operator'} text={'x'} />
+          <NumberButton buttonStyle={'operator'} text={'-'} />
+          <NumberButton buttonStyle={'operator'} text={'+'} />
+          <NumberButton buttonStyle={'operator'} text={'='} />
+        </div>
+      </div>  
+
+      
+      
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import './Display.css';
 
+const CalculatorDisplay = props => {
+    return (
+        <div>
+            <p className={props.buttonStyle}>{props.text}</p>
+        </div>
+    );
+}
 
+export default CalculatorDisplay;
